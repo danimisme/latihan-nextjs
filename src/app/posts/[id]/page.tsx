1e9c90291e5cd22366943b7a09aab8f818d4cd01
danimisme <msubhanr53@gmail.com>
2024-05-08 20:30:59 +0700

    get detail post data

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,13 +1,21 @@
+import { Post } from "@/model/post";
+import axios from "axios";
+
 export const metadata = {
   title: "Posts",
   description: "Posts",
 };
 
-export default function Page() {
+export default async function Page({ params }: { params: { id: string } }) {
+  const { data } = await axios.get<Post>(
+    "http://localhost:3001/posts/" + params.id
+  );
   return (
     <div>
-      <h1 className="font-bold text-2xl text-gray-600">Posts Detail</h1>
-      <div className="py-4 text-gray-600"></div>
+      <h1 className="font-bold text-2xl text-gray-600">Posts - {data.title}</h1>
+      <div className="py-4 text-gray-600">
+        <p>{data.description}</p>
+      </div>
     </div>
   );
 }
