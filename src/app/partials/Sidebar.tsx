0b9d9569c12d9a8b5386d9350052e6d37bb7b432
danimisme <msubhanr53@gmail.com>
2024-05-08 15:35:49 +0700

    modif sidebar

--- src/app/partials/Sidebar.tsx
+++ src/app/partials/Sidebar.tsx
@@ -1,8 +1,39 @@
+interface Menu {
+  label: string;
+  url: string;
+}
+
+const menus: Menu[] = [
+  {
+    label: "Home",
+    url: "/",
+  },
+  {
+    label: "About",
+    url: "/about",
+  },
+  {
+    label: "Posts",
+    url: "/posts",
+  },
+  {
+    label: "Contact",
+    url: "/contact",
+  },
+];
+
 const Sidebar = () => {
   return (
-    <>
-      <h1>Side Bar</h1>
-    </>
+    <div className="w-[18%] bg-gray-800 h-full flex justify-center p-4 py-10">
+      <ul>
+        <li className="mb-10 text-white font-bold text-xl">My Blog</li>
+        {menus.map((menu, index) => (
+          <li key={index} className="my-6 text-white">
+            {menu.label}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
