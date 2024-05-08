0bffebc6a1b985a0b5b2cc8223bb37396f351b17
danimisme <msubhanr53@gmail.com>
2024-05-08 15:44:01 +0700

    change footer style

--- src/app/partials/Footer.tsx
+++ src/app/partials/Footer.tsx
@@ -1,8 +1,8 @@
 const Footer = () => {
   return (
-    <>
-      <h1>Footer</h1>
-    </>
+    <footer className="w-full bg-gray-500 py-8 p-4 text-white">
+      <h1> Dans Blog @ 2024</h1>
+    </footer>
   );
 };
 
