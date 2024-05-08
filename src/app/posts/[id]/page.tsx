7c53e07b8e3a427f117a8ce900cdc6b734a9ddfc
danimisme <msubhanr53@gmail.com>
2024-05-08 19:36:49 +0700

    create detail post page

---
+++ src/app/posts/[id]/page.tsx
@@ -0,0 +1,13 @@
+export const metadata = {
+  title: "Posts",
+  description: "Posts",
+};
+
+export default function Page() {
+  return (
+    <div>
+      <h1 className="font-bold text-2xl text-gray-600">Posts Detail</h1>
+      <div className="py-4 text-gray-600"></div>
+    </div>
+  );
+}
